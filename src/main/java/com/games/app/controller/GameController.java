92b2b518a29e45787b1f470511578f31e2624ade
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2024-11-14 12:38:52 -0300

    GET game by id

--- src/main/java/com/games/app/controller/GameController.java
+++ src/main/java/com/games/app/controller/GameController.java
@@ -1,9 +1,11 @@
 package com.games.app.controller;
 
+import com.games.app.dto.GameDTO;
 import com.games.app.dto.GameMinDTO;
 import com.games.app.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
--- src/main/java/com/games/app/services/GameService.java
+++ src/main/java/com/games/app/services/GameService.java
@@ -1,12 +1,16 @@
 package com.games.app.services;
 
+import com.games.app.dto.GameDTO;
 import com.games.app.dto.GameMinDTO;
 import com.games.app.entities.Game;
 import com.games.app.repositories.GameRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -14,8 +18,29 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    //signfica que ele vai somente ler dados
+    @Transactional(readOnly=true)
+    public GameDTO findById(Long id){
+        // Verifica se o ID é nulo
+        if (id == null) {
+            throw new IllegalArgumentException("O ID não pode ser nulo");
+        }
+
+        // Busca o jogo no repositório usando o ID
+        Optional<Game> result = gameRepository.findById(id);
+
+        // Verifica se o jogo foi encontrado
+        if (result.isEmpty()) {
+            throw new EntityNotFoundException("Jogo com ID " + id + " não encontrado");
+        }
+
+        // Converte o jogo encontrado em DTO e retorna
+        return new GameDTO(result.get());
+    }
+
+    @Transactional(readOnly=true)
     public List<GameMinDTO> findAll(){
-       List<Game> result = gameRepository.findAll();
+       List<Game> result = gameRepository.findAll();  //objeto com todos os dados
        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
        return dto;
     }
